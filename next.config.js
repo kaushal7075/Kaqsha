29adaeb8bde1f91461bded192664750b84408f7a
Kaushal Rai <kaushalraiskt111@gmail.com>
2024-05-10 15:52:25 +0530

    api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@react-email/components",
+      "@react-email/tailwind",
+    ],
+  },
+};
 
 module.exports = nextConfig;
