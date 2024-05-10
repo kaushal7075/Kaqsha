79cc6a9aba31a87d09b62bb306fa10afa0249c40
Kaushal Rai <kaushalraiskt111@gmail.com>
2024-05-10 13:02:49 +0530

    resend

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,25 +1,52 @@
-import { render } from "@react-email/render";
-import WelcomeTemplate from "../../../emails";
+// import { render } from "@react-email/render";
+// import WelcomeTemplate from "../../../emails";
+// import { Resend } from "resend";
+// import React from "react";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function POST(request: Request, res: Response) {
+//   // rate limit
+//   // authorization
+
+//   const { email, userFirstname } = await request.json();
+
+//   const { data, error } = await resend.emails.send({
+//     from: "Kaqsha <info@kaqsha.com>",
+//     to: [email],
+//     subject: "Thank you",
+//     html: render(WelcomeTemplate({ userFirstname })),
+//   });
+
+//   if (error) {
+//     return Response.json(error);
+//   }
+
+//   return Response.json({ message: "Email sent successfully" });
+// }
+
+import { NextResponse } from "next/server";
+import * as React from "react";
 import { Resend } from "resend";
+import EmailTemplate from "../../../emails";
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request, res: Response) {
-  // rate limit
-  // authorization
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    console.log("BODY", body);
+    const { name, email, message } = body;
 
-  const { email, userFirstname } = await request.json();
+    const data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: "henriqueserres@gmail.com",
+      subject: `Hello!! ${name} visit your portfolio, Henrique Serres. And contact you.`,
+      react: EmailTemplate({ name, email, message }) as React.ReactElement,
+    });
 
-  const { data, error } = await resend.emails.send({
-    from: "Kaqsha <info@kaqsha.com>",
-    to: [email],
-    subject: "Thank you",
-    html: render(WelcomeTemplate({ userFirstname })),
-  });
-
-  if (error) {
-    return Response.json(error);
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
   }
-
-  return Response.json({ message: "Email sent successfully" });
 }
