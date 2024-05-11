072bd94037a81bc7aec4789a67b260363a6900d6
Kaushal Rai <kaushalraiskt111@gmail.com>
2024-05-11 15:26:42 +0530

    app

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,26 +1,54 @@
-import { render } from "@react-email/render";
-import WelcomeTemplate from "../../../emails";
-import { Resend } from "resend";
-import React from "react";
+// import { render } from "@react-email/render";
+// import WelcomeTemplate from "../../../emails";
+// import { Resend } from "resend";
+// import React from "react";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request, res: Response) {
-  // rate limit
-  // authorization
+// export async function POST(request: Request, res: Response) {
+//   // rate limit
+//   // authorization
 
-  const { email, userFirstname } = await request.json();
+//   const { email, userFirstname } = await request.json();
 
-  const { data, error } = await resend.emails.send({
-    from: "Kaqsha <info@kaqsha.com>",
-    to: [email],
-    subject: "Thank you",
-    html: render(WelcomeTemplate({ userFirstname })),
+//   const { data, error } = await resend.emails.send({
+//     from: "Kaqsha <info@kaqsha.com>",
+//     to: [email],
+//     subject: "Thank you",
+//     html: render(WelcomeTemplate({ userFirstname })),
+//   });
+
+//   if (error) {
+//     return Response.json(error);
+//   }
+
+//   return Response.json({ message: "Email sent successfully" });
+// }
+
+import { NextResponse } from "next/server";
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const RESEND_API_KEY = "re_EWgNu5B6_7BEwEd2uyFaZtjtqfgoALHfL";
+
+export async function POST() {
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: "Acme <infp@kaqsha.com>",
+      to: ["kaushal.rai@evonix.co"],
+      subject: "hello world",
+      html: "<strong>it works!</strong>",
+    }),
   });
 
-  if (error) {
-    return Response.json(error);
+  if (res.ok) {
+    const data = await res.json();
+    return NextResponse.json(data);
   }
-
-  return Response.json({ message: "Email sent successfully" });
 }
